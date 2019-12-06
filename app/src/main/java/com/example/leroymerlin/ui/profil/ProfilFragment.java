a29d1f2307498e1b87b1ee3a138b0338cae32e21
Axel <axel.coulon99@orange.fr>
2019-12-06 02:46:42 +0100

    Page panier : les linearLayout des annonces se déploient et rétrécissent dynamiquement

--- app/src/main/java/com/example/leroymerlin/ui/profil/ProfilFragment.java
+++ app/src/main/java/com/example/leroymerlin/ui/profil/ProfilFragment.java
@@ -1,8 +1,9 @@
 package com.example.leroymerlin.ui.profil;
 
 import android.annotation.SuppressLint;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -23,7 +24,10 @@ public class ProfilFragment extends Fragment {
 
     private ProfilViewModel notificationsViewModel;
     private LinearLayout layoutProfil;
-    private LinearLayout linearLayoutOuvert;
+    private LinearLayout linearLayoutOpen;
+    private ImageView imageViewRotated; // sauvegarde l'imageView qui a été tournée
+    private ScrollView scrollViewOpen;
+    private boolean seeInformations = true; // booléen qui sert à éviter d'activer deux défilements à la fois
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -54,7 +58,7 @@ public class ProfilFragment extends Fragment {
         // Création d'un scrollView principal qui va contenir toutes les annonces
         ScrollView scrollViewPrincipal = new ScrollView(getContext());
 
-        LinearLayout linearLayoutPrincipal = new LinearLayout(getContext());
+        final LinearLayout linearLayoutPrincipal = new LinearLayout(getContext());
         layoutProfil.addView(scrollViewPrincipal);
         linearLayoutPrincipal.setOrientation(LinearLayout.VERTICAL);
         scrollViewPrincipal.addView(linearLayoutPrincipal);
@@ -62,45 +66,55 @@ public class ProfilFragment extends Fragment {
         // pour chaque annonce, on crée une "fiche"
         for (int i = 0 ; i < 5 ; i++) {
             // Création du scroll view de la fiche
-            ScrollView scrollViewAd = new ScrollView(getContext());
+            final ScrollView scrollViewAd = new ScrollView(getContext());
             LinearLayout.LayoutParams scrollViewParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
             scrollViewParams.setMargins(0,30,0,0);
             scrollViewAd.setLayoutParams(scrollViewParams);
             linearLayoutPrincipal.addView(scrollViewAd);
 
             // On crée un linearLayout pour le scrollview
-            LinearLayout linearLayoutScrollViewAd = new LinearLayout(getContext());
+            final LinearLayout linearLayoutScrollViewAd = new LinearLayout(getContext());
             linearLayoutScrollViewAd.setPadding(50,5,50,5);
             linearLayoutScrollViewAd.setOrientation(LinearLayout.VERTICAL);
             scrollViewAd.addView(linearLayoutScrollViewAd);
 
             // On crée le premier linearLayout qui contient les infos principales
             final LinearLayout linearLayoutAdPrincipal = new LinearLayout(getContext());
-            linearLayoutAdPrincipal.setPadding(10,0,10,15);
+            linearLayoutAdPrincipal.setPadding(40,20,40,30);
             linearLayoutAdPrincipal.setOrientation(LinearLayout.VERTICAL);
             linearLayoutAdPrincipal.setBackgroundResource(R.color.colorPrimary);
             linearLayoutScrollViewAd.addView(linearLayoutAdPrincipal);
 
             // on y ajoute les informations principales
             TextView nameAd = new TextView(getContext());
+            //LinearLayout.LayoutParams nameAdParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            //nameAdParams.setMargins();
             nameAd.setTextColor(getResources().getColor(R.color.colorTextWhite));
             nameAd.setText("Engineer BI - STAGE");
             nameAd.setTextSize(convertSpToPixel(10));
             Log.i("Engineer BI - STAGE", nameAd.getText().toString());
             linearLayoutAdPrincipal.addView(nameAd);
+
+            TextView textViewMission = new TextView(getContext());
+            textViewMission.setTextColor(getResources().getColor(R.color.colorTextWhite));
+            textViewMission.setText("Mission");
+            textViewMission.setTextSize(convertSpToPixel(7));
+            textViewMission.setTypeface(textViewMission.getTypeface(), Typeface.BOLD);
+            linearLayoutAdPrincipal.addView(textViewMission);
+
             TextView missionsAd = new TextView(getContext());
             missionsAd.setTextColor(getResources().getColor(R.color.colorTextWhite));
-            missionsAd.setText("Mission :\n" +
-                    "Au sein de l’équipe DATA Leroy Merlin, tes missions seront les suivantes\n" +
+            missionsAd.setText("Au sein de l’équipe DATA Leroy Merlin, tes missions seront les suivantes\n" +
                     "Tu travailles au contact des équipes métiers ; \n" +
                     "Tu proposes des solutions techniques innovantes ; \n" +
                     "Tu modélises et construis le socle Data ; \n" +
                     "Tu conçois et construis les solutions de restitutions. ");
             linearLayoutAdPrincipal.addView(missionsAd);
-            ImageView imageViewMore = new ImageView(getContext());
+            final ImageView imageViewMore = new ImageView(getContext());
             LinearLayout.LayoutParams imageViewParams = new LinearLayout.LayoutParams(40, 40);
             imageViewParams.weight = 1;
             imageViewParams.gravity = Gravity.CENTER_HORIZONTAL;
+            imageViewParams.setMargins(0,20,0,0);
             imageViewMore.setLayoutParams(imageViewParams);
             imageViewMore.setImageResource(R.drawable.ic_arrow_down);
             linearLayoutAdPrincipal.addView(imageViewMore);
@@ -108,12 +122,11 @@ public class ProfilFragment extends Fragment {
 
             // On crée le second linearLayout qui contient les infos complémentaires
             final LinearLayout linearLayoutAdSecondary = new LinearLayout(getActivity());
-            linearLayoutAdSecondary.setPadding(10,0,10,0);
+            linearLayoutAdSecondary.setPadding(25,0,25,0);
             linearLayoutAdSecondary.setBackgroundResource(R.color.colorPrimaryDark);
             linearLayoutAdSecondary.setOrientation(LinearLayout.VERTICAL);
             linearLayoutScrollViewAd.addView(linearLayoutAdSecondary);
-            linearLayoutAdSecondary.setVisibility(View.GONE);
-            TextView description = new TextView(getContext());
+            final TextView description = new TextView(getContext());
             description.setTextColor(getResources().getColor(R.color.colorTextWhite));
             description.setText("Voici les technos que vous devrez utiliser :\n" +
                     "Java\n" +
@@ -126,23 +139,139 @@ public class ProfilFragment extends Fragment {
                     "Tu es agile et force de propositions.\n\n" +
                     "Interessé ? Contactez marine.coisne@gmail.com");
             linearLayoutAdSecondary.addView(description);
+            linearLayoutAdSecondary.setVisibility(View.GONE);
 
             linearLayoutAdPrincipal.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (linearLayoutAdSecondary.getVisibility() == View.GONE) {
-                        linearLayoutAdSecondary.setVisibility(View.VISIBLE);
-                        if (null != linearLayoutOuvert) {
-                            linearLayoutOuvert.setVisibility(View.GONE);
+                    // on vérifie qu'il n'y a pas d'action en cours (par exemple un linearLayout qui est déjà en train de s'agrandir ou de rétrécir)
+                    if (seeInformations) {
+                        seeInformations = false;
+                        // si les informations complémentaires de l'offre cliquée ne sont pas visibles actuellement
+                        if (linearLayoutAdSecondary.getVisibility() == View.GONE) {
+                            final int linearHeight = scrollViewAd.getHeight();
+                            LinearLayout.LayoutParams scrollViewParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, linearHeight);
+                            scrollViewParams.setMargins(0, 30, 0, 0);
+                            scrollViewAd.setLayoutParams(scrollViewParams);
+                            linearLayoutAdSecondary.setVisibility(View.VISIBLE);
+                            linearLayoutAdSecondary.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    int maxSizeInformations = linearLayoutAdSecondary.getHeight();
+                                    Log.i("Taille MAXIMALE", "" + maxSizeInformations);
+                                    //LinearLayout.LayoutParams scrollViewParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0);
+                                    //linearLayoutAdSecondary.setLayoutParams(scrollViewParams);
+                                    for (int i = 0; i < maxSizeInformations; i++) {
+                                        Handler handler = new Handler();
+                                        final int finalI = i;
+                                        handler.postDelayed(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                //Log.i("Taille", finalI +"");
+                                                LinearLayout.LayoutParams scrollViewParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, linearHeight + finalI);
+                                                scrollViewParams.setMargins(0, 30, 0, 0);
+                                                scrollViewAd.setLayoutParams(scrollViewParams);
+                                            }
+                                        }, (i) / 2);
+                                    }
+                                    Handler handler = new Handler();
+                                    handler.postDelayed(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            seeInformations = true;
+                                        }
+                                    }, (maxSizeInformations+1) / 2);
+                                }
+                            });
+                            imageViewMore.setRotation(180);
+
+                            if (null != linearLayoutOpen) {
+                                closeLinearInformations(scrollViewOpen, linearLayoutOpen, imageViewRotated);
+                                //imageViewRotated.setRotation(0);
+                                //linearLayoutOpen.setVisibility(View.GONE);
+                            }
+                            scrollViewOpen = scrollViewAd;
+                            imageViewRotated = imageViewMore;
+                            linearLayoutOpen = linearLayoutAdSecondary;
+                        } else {
+                            final int linearHeight = scrollViewAd.getHeight();
+                            Log.i("Test taille linear", linearHeight + "");
+
+                            final int maxSizeInformations = linearLayoutAdSecondary.getHeight();
+                            Log.i("Taille MAXIMALE", "" + maxSizeInformations);
+                            //LinearLayout.LayoutParams scrollViewParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0);
+                            //linearLayoutAdSecondary.setLayoutParams(scrollViewParams);
+                            for (int i = maxSizeInformations; i > 0; i--) {
+                                Handler handler = new Handler();
+                                final int finalI = i;
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        LinearLayout.LayoutParams scrollViewParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, linearHeight - maxSizeInformations + finalI);
+                                        scrollViewParams.setMargins(0, 30, 0, 0);
+                                        scrollViewAd.setLayoutParams(scrollViewParams);
+                                    }
+                                }, (maxSizeInformations - i) / 2);
+                            }
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    linearLayoutAdSecondary.setVisibility(View.GONE);
+                                    seeInformations = true;
+                                }
+                            }, (maxSizeInformations + 1) / 2);
+
+                            imageViewMore.setRotation(0);
+                            imageViewRotated = null;
+                            linearLayoutOpen = null;
                         }
-                        linearLayoutOuvert = linearLayoutAdSecondary;
-                    } else {
-                        linearLayoutAdSecondary.setVisibility(View.GONE);
-                        linearLayoutOuvert = null;
+                    }
+                }
+            });
+
+            linearLayoutAdSecondary.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (seeInformations) {
+                        closeLinearInformations(scrollViewAd, linearLayoutAdSecondary, imageViewMore);
+                        imageViewRotated = null;
+                        linearLayoutOpen = null;
                     }
                 }
             });
         }
+    }
+
+    private void closeLinearInformations(final ScrollView scrollView, final LinearLayout linearLayout, ImageView imageView) {
+        final int linearHeight = scrollView.getHeight();
+        Log.i("Test taille linear", linearHeight +"");
+
+        final int maxSizeInformations = linearLayout.getHeight();
+        Log.i("Taille MAXIMALE", "" + maxSizeInformations);
+        //LinearLayout.LayoutParams scrollViewParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0);
+        //linearLayoutAdSecondary.setLayoutParams(scrollViewParams);
+        for (int i = maxSizeInformations; i > 0 ; i--) {
+            Handler handler = new Handler();
+            final int finalI = i;
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    LinearLayout.LayoutParams scrollViewParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, linearHeight - maxSizeInformations + finalI);
+                    scrollViewParams.setMargins(0,30,0,0);
+                    scrollView.setLayoutParams(scrollViewParams);
+                }
+            }, (maxSizeInformations - i)/2);
+        }
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                linearLayout.setVisibility(View.GONE);
+                seeInformations = true;
+            }
+        }, (maxSizeInformations +1)/2);
+        imageView.setRotation(0);
     }
 
     public float convertSpToPixel(float sp){
