e623f2e5cdcbaf3cb1d7854ee8d52ffe4a6311a3
Maxime Maurin <maxime.maurin53@gmail.com>
2019-12-06 06:25:28 +0100

    PreferencesManager

---
+++ app/src/main/java/com/example/leroymerlin/utils/PreferencesManager.java
@@ -0,0 +1,73 @@
+package com.example.leroymerlin.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PreferencesManager {
+
+    public static final String PREFS_NAME = "Leroy Merlin";
+    public static Context contextPref;
+
+    public static void setContext(Context context){
+        contextPref = context;
+    }
+
+    public static List<Integer> getAllPrefAds(){
+        List<Integer> list = new ArrayList<>();
+
+        SharedPreferences settings = contextPref.getSharedPreferences(PREFS_NAME, 0);
+        if (settings.contains("prefAds")){
+            String[] prefAds = settings.getString("prefAds", "").split(",");
+            for (String pref : prefAds){
+                list.add(Integer.parseInt(pref));
+            }
+        }
+        return list;
+    }
+
+    public static List<Integer> getAllUninterestingAds(){
+        List<Integer> list = new ArrayList<>();
+
+        SharedPreferences settings = contextPref.getSharedPreferences(PREFS_NAME, 0);
+        if (settings.contains("uninAds")){
+            String[] uninAds = settings.getString("uninAds", "").split(",");
+            for (String unin : uninAds){
+                list.add(Integer.parseInt(unin));
+            }
+        }
+        return list;
+    }
+
+    public static void addPrefAd(int pid){
+        List<Integer> list = getAllPrefAds();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Integer id : list){
+            stringBuilder.append(id+",");
+        }
+        stringBuilder.substring(0, stringBuilder.length()-2);
+
+        SharedPreferences settings = contextPref.getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("predAds", stringBuilder.toString());
+        editor.apply();
+    }
+
+    public static void addUninterestedAd(int pid){
+        List<Integer> list = getAllUninterestingAds();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Integer id : list){
+            stringBuilder.append(id+",");
+        }
+        stringBuilder.substring(0, stringBuilder.length()-2);
+
+        SharedPreferences settings = contextPref.getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("uninAds", stringBuilder.toString());
+        editor.apply();
+    }
+
+
+}
